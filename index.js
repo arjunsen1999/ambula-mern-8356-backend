763e28eca9bee667b79f5b5862f981f1615dec44
arjunsen1999 <arjunsen717@gmail.com>
2023-07-15 00:35:50 +0530

    added backend

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT;
+const { connection } = require("./config/db");
+const { ErrorHandler } = require("./middleware/ErrorHandling.middleware");
+const { AppError } = require("./class/AppError");
+
+//All Routes import here
+
+// Global Middleware
+app.use(express.json());
+app.use(cors());
+
+// All Routes
+
+// if Routes are not exists
+app.all("*", (req, res, next) => {
+  next(new AppError(`${req.originalUrl} <- this Route not found!`, 404));
+});
+
+// Error handling middleware
+app.use(ErrorHandler);
+
+// listening server and connection
+app.listen(PORT, () => {
+    connection();
+  console.log({ server: `http://localhost:${PORT}` });
+});
